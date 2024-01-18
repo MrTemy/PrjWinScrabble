461db0766ba3f9780f9895759f7685978731eb58
MrTemy <emanueljean69@gmail.com>
2024-01-18 16:20:22 +0100

    first commit

---
+++ WinScrabble/frmScrabble.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace WinScrabble
+{
+    partial class frmScrabble
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmScrabble));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMot = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnAutre = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPoints = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTotalPoints = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNbrMots = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.rtxtMot = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(32, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Saisir un mot";
+            // 
+            // txtMot
+            // 
+            this.txtMot.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtMot.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMot.ForeColor = System.Drawing.Color.Black;
+            this.txtMot.Location = new System.Drawing.Point(145, 35);
+            this.txtMot.Name = "txtMot";
+            this.txtMot.Size = new System.Drawing.Size(215, 23);
+            this.txtMot.TabIndex = 1;
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnOK.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.btnOK.FlatAppearance.BorderSize = 0;
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.ForeColor = System.Drawing.Color.Black;
+            this.btnOK.Location = new System.Drawing.Point(262, 76);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(88, 30);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnAutre
+            // 
+            this.btnAutre.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnAutre.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.btnAutre.FlatAppearance.BorderSize = 0;
+            this.btnAutre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAutre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAutre.ForeColor = System.Drawing.Color.Black;
+            this.btnAutre.Location = new System.Drawing.Point(152, 76);
+            this.btnAutre.Name = "btnAutre";
+            this.btnAutre.Size = new System.Drawing.Size(87, 30);
+            this.btnAutre.TabIndex = 3;
+            this.btnAutre.Text = "Autre mot";
+            this.btnAutre.UseVisualStyleBackColor = false;
+            this.btnAutre.Click += new System.EventHandler(this.btnAutre_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(32, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ce mot rapporte";
+            // 
+            // txtPoints
+            // 
+            this.txtPoints.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtPoints.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPoints.ForeColor = System.Drawing.Color.Black;
+            this.txtPoints.Location = new System.Drawing.Point(188, 153);
+            this.txtPoints.Name = "txtPoints";
+            this.txtPoints.ReadOnly = true;
+            this.txtPoints.Size = new System.Drawing.Size(93, 23);
+            this.txtPoints.TabIndex = 5;
+            this.txtPoints.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(308, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "points";
+            // 
+            // txtTotalPoints
+            // 
+            this.txtTotalPoints.Location = new System.Drawing.Point(188, 191);
+            this.txtTotalPoints.Name = "txtTotalPoints";
+            this.txtTotalPoints.ReadOnly = true;
+            this.txtTotalPoints.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalPoints.TabIndex = 7;
+            this.txtTotalPoints.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(53, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cumul points";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(315, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "points";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(53, 227);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "nombre de mots";
+            // 
+            // txtNbrMots
+            // 
+            this.txtNbrMots.Location = new System.Drawing.Point(188, 224);
+            this.txtNbrMots.Name = "txtNbrMots";
+            this.txtNbrMots.ReadOnly = true;
+            this.txtNbrMots.Size = new System.Drawing.Size(100, 20);
+            this.txtNbrMots.TabIndex = 13;
+            this.txtNbrMots.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(315, 227);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "mot(s)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Location = new System.Drawing.Point(459, 22);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Vos mots";
+            // 
+            // rtxtMot
+            // 
+            this.rtxtMot.Enabled = false;
+            this.rtxtMot.Location = new System.Drawing.Point(431, 38);
+            this.rtxtMot.Name = "rtxtMot";
+            this.rtxtMot.ReadOnly = true;
+            this.rtxtMot.Size = new System.Drawing.Size(112, 180);
+            this.rtxtMot.TabIndex = 17;
+            this.rtxtMot.Text = "";
+            // 
+            // frmScrabble
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::WinScrabble.Properties.Resources.FondScrabble;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(571, 282);
+            this.Controls.Add(this.rtxtMot);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtNbrMots);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTotalPoints);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPoints);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAutre);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtMot);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmScrabble";
+            this.Text = "JEU DU SCRABBLE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMot;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnAutre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPoints;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTotalPoints;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNbrMots;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RichTextBox rtxtMot;
+    }
+}
+
---
+++ WinScrabble/frmScrabble.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinScrabble
+{
+    public partial class frmScrabble : Form
+    {
+        // attributs de la classe frmScrabble (variables globales)
+        // utiles pour les évolutions EVO01 et EVO02
+        int totalPoints = 0;                // nombre de points du joueur
+        int nbMots = 0, ptMotMax = 0;                     // nombre de mots proposés par le joueur
+        string[] lesMots = new string[10];  // mots proposés par le joueur
+
+        public frmScrabble()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string leMot = txtMot.Text.ToUpper();
+            lesMots[nbMots] = leMot;
+            int nbPoints = Utilitaire.PointsMot(leMot);
+            txtPoints.Text = nbPoints.ToString();
+            totalPoints = totalPoints + nbPoints;
+            txtTotalPoints.Text = totalPoints.ToString();
+            nbMots++;
+            txtNbrMots.Text = nbMots.ToString();
+            rtxtMot.Visible = true;
+            btnOK.Enabled = false;
+            foreach (string mot in lesMots)
+            {
+                if (nbPoints > ptMotMax)
+                {
+                    //couleur pr meilleur mot
+                    rtxtMot.AppendText(mot);
+                }
+                else
+                {
+                    rtxtMot.AppendText(mot);
+                }
+            }
+        }
+
+
+        private void btnAutre_Click(object sender, EventArgs e)
+        {
+            txtMot.Clear();
+            txtPoints.Clear();
+            txtMot.Focus();
+            btnOK.Enabled = true;
+            if(nbMots==10)
+            {
+                btnOK.Enabled = false;
+                btnAutre.Enabled = false;
+            }
+        }
+    }
+}
